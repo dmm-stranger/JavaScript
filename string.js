c48ec7869d7ba3e2dc20f7296232f4723f0bcde8
14HAK <dulonmolla88@gmail.com>
2026-05-11 07:41:22 +0600

    some test and practice js String methods

---
+++ string.js
@@ -0,0 +1,20 @@
+// javaScript charAt() and at():
+const myStr = 'hello world';
+// console.log(myStr.charAt(0));
+// console.log(myStr.at(-1));
+
+// js search methods:
+const studentStr = 'my name is Dulon Mahadi';
+// console.log(studentStr.includes('Du'));
+// console.log(studentStr.startsWith('Du'));
+// console.log(studentStr.endsWith('Di'));
+
+// js extract / slice methods:
+const teacherStr = 'my name is Dulon Mahadi';
+// console.log(teacherStr.slice(3, 10));
+// console.log(teacherStr.substring(0, 10));
+
+// js replace / modify methods:
+const youngerStr = 'colors, are, blindness';
+console.log(youngerStr.replace('colors', 'madness'));
+console.log(youngerStr.split(','));
